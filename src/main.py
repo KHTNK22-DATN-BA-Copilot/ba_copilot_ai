3b315f32c0b6339f8f0f54be05d2a807ebf010e6
Ma Cat Huynh <uynhhuc810@gmail.com>
2025-09-29 00:55:53 +0700

    OPS-13 small renaming

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def create_app() -> FastAPI:
     # Configure CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Configure based on your requirements
+        allow_origins=["*"],  # Configure based on requirements later
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
