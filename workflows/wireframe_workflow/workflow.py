2f0fe89c146df910613989bdbde177c4cced397c
Ma Cat Huynh <uynhhuc810@gmail.com>
2025-11-04 19:58:26 +0700

    OPS-266 implemented crude HTML-CSS wireframe generation

--- workflows/wireframe_workflow/workflow.py
+++ workflows/wireframe_workflow/workflow.py
@@ -4,8 +4,9 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from figma_mcp import generate_figma_wireframe
-from models.wireframe import WireframeOutput, WireframeResponse
-from typing import TypedDict
+from models.wireframe import WireframeOutput, WireframeResponse, WireframeHTMLCSSOutput
+from typing import Dict, TypedDict
+from .html_css_generator import generate_html_css
 
 class WireframeState(TypedDict):
     user_message: str
@@ -19,7 +20,7 @@ def generate_wireframe(state: WireframeState) -> WireframeState:
         wireframe_response = WireframeResponse(**result)
         output = WireframeOutput(type="wireframe", response=wireframe_response)
 
-        return {"response": output.model_dump()["response"]}
+        return {"response": output.model_dump()["response"]} # pyright: ignore[reportReturnType]
 
     except Exception as e:
         print(f"Error generating wireframe: {e}")
@@ -30,17 +31,39 @@ def generate_wireframe(state: WireframeState) -> WireframeState:
                 "editable": False,
                 "description": f"Error generating wireframe: {str(e)}"
             }
-        }
+        } # pyright: ignore[reportReturnType]
 
-# Build LangGraph pipeline for Wireframe
+def generate_wireframe_html_css(state: WireframeState) -> WireframeState:
+    """Generate wireframe with HTML, CSS"""
+    try:
+        result = generate_html_css(state["user_message"])
+
+        wireframe_response = WireframeHTMLCSSOutput(response=result)
+        
+        return {"response": wireframe_response.model_dump()["response"]} # pyright: ignore[reportReturnType]
+
+    except Exception as e:
+        print(f"Error creating HTML, CSS wireframe : {e}")
+        import traceback
+        traceback.print_exc()
+        # Fallback response
+        return {
+            "response": {
+                "content": f"Error creating HTML, CSS wireframe: {str(e)}"
+            }
+        } # pyright: ignore[reportReturnType]# Build LangGraph pipeline for Wireframe
 workflow = StateGraph(WireframeState)
 
 # Add node
-workflow.add_node("generate_wireframe", generate_wireframe)
+# workflow.add_node("generate_wireframe", generate_wireframe)
+workflow.add_node("generate_wireframe_html_css", generate_wireframe_html_css)
 
 # Set entry point and finish
-workflow.set_entry_point("generate_wireframe")
-workflow.add_edge("generate_wireframe", END)
+# workflow.set_entry_point("generate_wireframe")
+# workflow.add_edge("generate_wireframe", END)
+
+workflow.set_entry_point("generate_wireframe_html_css")
+workflow.add_edge("generate_wireframe_html_css", END)
 
 # Compile graph
 wireframe_graph = workflow.compile()
