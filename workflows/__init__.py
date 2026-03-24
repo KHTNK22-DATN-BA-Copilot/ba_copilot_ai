b45cce11201dba05f093ff896de8af017437576c
Ma Cat Huynh <uynhhuc810@gmail.com>
2026-03-24 21:01:59 +0700

    removed deprecated wireframe workflow

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -6,7 +6,7 @@ from .srs_workflow import srs_graph
 from .class_diagram_workflow import class_diagram_graph
 from .usecase_diagram_workflow import usecase_diagram_graph
 from .activity_diagram_workflow import activity_diagram_graph
-from .wireframe_workflow import wireframe_graph
+# from .wireframe_workflow import wireframe_graph
 from .stakeholder_register_workflow import stakeholder_register_graph
 from .high_level_requirements_workflow import high_level_requirements_graph
 from .requirements_management_plan_workflow import requirements_management_plan_graph
@@ -35,7 +35,7 @@ __all__ = [
     "class_diagram_graph",
     "usecase_diagram_graph",
     "activity_diagram_graph",
-    "wireframe_graph",
+    # "wireframe_graph",
     "stakeholder_register_graph",
     "high_level_requirements_graph",
     "requirements_management_plan_workflow",
