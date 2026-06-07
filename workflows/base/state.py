05b6da3b3015e1811c70ff70f37217eceabb4ed9
OriginalNVK <nguyenvankhanh27072004@gmail.com>
2026-06-07 11:33:56 +0700

    fix: change logic to get chunk document follow document constraint

--- workflows/base/state.py
+++ workflows/base/state.py
@@ -4,11 +4,9 @@ from typing import TypedDict, Optional, List
 class BaseDocumentState(TypedDict):
     user_message: str
     response: dict
-
     content_id: Optional[str]
-
-    storage_paths: Optional[List]
     extracted_text: Optional[str]
     chat_context: Optional[str]
-
     document_format: Optional[str]
+    project_id: Optional[int]
+    document_constraint: Optional[List[str]]
